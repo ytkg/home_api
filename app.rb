57bda753a0f7adbc18cfddc22313ffc70f596050
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-11-05 23:28:08 +0900

    Switch from nature remo to switchbot

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'nature_remo_api'
+require 'switchbot'
 
 get '/:device/:switch' do |d, s|
   send("#{d}_#{s}")
@@ -10,13 +10,13 @@ rescue => e
 end
 
 def client
-  NatureRemoApi::Client.new(access_token: ENV['NATURE_REMO_API_TOKEN'])
+  Switchbot::Client.new(ENV['SWITCHBOT_API_TOKEN'])
 end
 
 def light_on
-  client.send_signal(signal_id: ENV['NATURE_REMO_LIGHT_ON_SIGNAL'])
+  client.device(ENV['SWITCHBOT_LIGHT_DEVICE_ID']).on
 end
 
 def light_off
-  client.send_signal(signal_id: ENV['NATURE_REMO_LIGHT_OFF_SIGNAL'])
+  client.device(ENV['SWITCHBOT_LIGHT_DEVICE_ID']).off
 end
