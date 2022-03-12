9d52828bce89997d6570e95d7411825344bf95d2
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-03-12 22:58:31 +0900

    Add router reboot api

--- app.rb
+++ app.rb
@@ -20,3 +20,10 @@ end
 def light_off
   client.device(ENV['SWITCHBOT_LIGHT_DEVICE_ID']).off
 end
+
+def router_reboot
+  return if ENV['APP_ENV'] != 'production'
+
+  uri = URI.parse("http://#{ENV['ROUTER_USERNAME']}:#{ENV['ROUTER_PASSWORD']}@#{ENV['ROUTER_IP']}/index.cgi/reboot_main_set")
+  Net::HTTP.post_form(uri, { 'SESSION_ID' => ENV['ROUTER_SESSION_ID'] })
+end
