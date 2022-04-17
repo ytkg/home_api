e6fcd919637730cb3c08ff8535a7aaa1f2e008fd
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-04-17 23:09:52 +0900

    Add get nature_remo_e/measured_instantaneous

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,11 +2,19 @@ require './app'
 
 RSpec.describe 'app' do
   before do
+    allow(NatureRemoE::Client).to receive_message_chain(:new, :measured_instantaneous).and_return(123)
     device_mock = double('Switchbot::Device')
     allow(Switchbot::Client).to receive_message_chain(:new, :device).and_return(device_mock)
     allow(device_mock).to receive(:on)
     allow(device_mock).to receive(:off)
     allow(Net::HTTP).to receive(:post_form)
+  end
+
+  it '/nature_remo_e/measured_instantaneous' do
+    get '/nature_remo_e/measured_instantaneous'
+
+    expect(last_response.status).to eq 200
+    expect(last_response.body).to eq({ value: 123 }.to_json)
   end
 
   it '/light/on' do
